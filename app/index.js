baeb17354a4964664852131e2d8e84a70c82943f
Tim Arioto <timarioto@gmail.com>
2017-03-08 13:34:14 -0800

    Set up react webpack babel npm

---
+++ app/index.js
@@ -0,0 +1,17 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const HelloWorld = React. createClass({
+  render: function () {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+);
