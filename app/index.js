741bb6a9cb1080012ceb1ee88dd8b78ae9581492
Tim Arioto <timarioto@gmail.com>
2017-03-08 14:01:25 -0800

    Add avatar component

--- app/index.js
+++ app/index.js
@@ -1,17 +1,51 @@
+const USER_DATA = {
+  name: 'Tim Arioto',
+  username: 'tarioto',
+  image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+}
+
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const HelloWorld = React. createClass({
+const ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+})
+
+const ProfileLink = React.createClass({
   render: function () {
     return (
       <div>
-        Hello World
+        <a href={'https://www.github.com/' + this.props.username}>
+        {this.props.username}
+        </a>
+      </div>
+    )
+  }
+})
+
+const ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div> {this.props.name} </div>
+    )
+  }
+})
+
+const Avatar = React. createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username}/>
       </div>
     )
   }
 })
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA}/>,
   document.getElementById('app')
 );
