a3e095ca34cfd242719fb9f44b2f19f54e96f9f1
Tim Arioto <timarioto@gmail.com>
2017-03-08 14:01:56 -0800

    Add Link component

--- app/index.js
+++ app/index.js
@@ -13,13 +13,29 @@ const ProfilePic = React.createClass({
   }
 })
 
+const Link = React.createClass({
+  changeURL: function () {
+    window.location.replace{this.props.href}
+  }
+  render: function () {
+    return (
+      <span
+        style={{color: 'blue', cursor: 'pointer'}}
+        onclick={this.changeURL}
+        >
+        {this.props.children}
+      </span>
+    )
+  }
+})
+
 const ProfileLink = React.createClass({
   render: function () {
     return (
       <div>
-        <a href={'https://www.github.com/' + this.props.username}>
+        <Link href={'https://www.github.com/' + this.props.username}>
         {this.props.username}
-        </a>
+        </Link>
       </div>
     )
   }
